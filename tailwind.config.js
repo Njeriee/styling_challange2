838482af19ce435376019e292613158758217196
njeriwachira <savelinnjeri@gmail.com>
2022-05-02 13:09:35 +0300

    home section,section one and section two styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: [],
+  content: [ './index.html'],
   theme: {
-    extend: {},
+    screens: {
+      'xs': '430px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      'transparent': 'transparent',
+      'primary': '#5ECD76',
+      'white': '#ffffff',
+      'black': 'black',
+      'grey': 'grey',
+      'orange': '#EA580C',
+    },
+    extend: {
+      backgroundImage: {
+        'split-white-green': "linear-gradient(to bottom, white 25%, #5ECD76 0%);"
+      }
+    }
   },
   plugins: [],
 }
